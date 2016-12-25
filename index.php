2636bbdc6cbc0c4db729872734125b84b358edd2
Frederik Van Herbruggen <Frederik.vanh@gmail.com>
2016-12-25 13:53:29 +0100

    added routes for admin pages

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
  */
 session_start();
 $_SESSION['pleb'] = "ayy";
-use \Psr\Http\Message\ServerRequestInterface as req;
-use \Psr\Http\Message\ResponseInterface as resp;
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
 use \Respect\Validation\Validator as v;
 
 require "bootstrap.php";
@@ -35,7 +35,7 @@ $app = new \Slim\App([
    ]
 ]);
 
-$app->get('/', function (req $req, resp $resp) use ($twig){
+$app->get('/', function (Request $req, Response $resp) use ($twig){
     $twigVar = array(
         "pageTitle" => "Home"
     );
@@ -44,7 +44,7 @@ $app->get('/', function (req $req, resp $resp) use ($twig){
    return $resp->getBody()->write($twig->render('index.html.twig', $twigVar));
 });
 
-$app->get('/work', function (req $req, resp $resp) use ($twig){
+$app->get('/work', function (Request $req, Response $resp) use ($twig){
     $twigVar = array(
         "pageTitle" => "Work",
         "spotlightL" => "",
@@ -57,7 +57,7 @@ $app->get('/work', function (req $req, resp $resp) use ($twig){
     return $resp->getBody()->write($twig->render('work.html.twig', $twigVar));
 });
 
-$app->get('/gallery', function (req $req, resp $resp) use ($twig){
+$app->get('/gallery', function (Request $req, Response $resp) use ($twig){
     //todo: get all images
     $twigVar = array(
         "pageTitle" => "Gallery"
@@ -68,7 +68,7 @@ $app->get('/gallery', function (req $req, resp $resp) use ($twig){
     return $resp->getBody()->write($twig->render('gallery.html.twig', $twigVar));
 });
 
-$app->get('/gallery/{cat}', function (req $req, resp $resp) use ($twig){
+$app->get('/gallery/{cat}', function (Request $req, Response $resp) use ($twig){
     $cat = $req->getAttribute("cat");
     $twigVar = array(
         "pageTitle" => "Gallery"
@@ -80,32 +80,32 @@ $app->get('/gallery/{cat}', function (req $req, resp $resp) use ($twig){
     return $resp->getBody()->write($twig->render('gallery.html.twig', $twigVar));
 });
 
-$app->post('/gallery', function (req $req, resp $resp) use ($artRepo){
+$app->post('/gallery', function (Request $req, Response $resp) use ($artRepo){
     //todo:do upload
 });
 
-$app->get('/login', function (req $req, resp $resp) use ($twig, $securityService){
+$app->get('/login', function (Request $req, Response $resp) use ($twig, $securityService){
     return $resp->getBody()->write($twig->render('admin.html.twig', array(
         "CSRFtoken" => $securityService->create_token_tag() //not actually required for a login form
     )));
 });
 
-$app->post('/login', function (req $req, resp $resp) use ($userRepo){
+$app->post('/login', function (Request $req, Response $resp) use ($userRepo){
     $data = $req->getParsedBody();
     if(!isset($data['username']) || !isset($data['password'])) return $response = $resp->withStatus(418);
     $auth = new AppBundle\controllers\AuthenticationController($userRepo);
     return $resp->getBody()->write(json_encode($auth->checkLogin($data['username'], $data['password'])));
 });
 
-$app->get('/logout', function (req $req, resp $resp) use ($twig, $securityService) {
+$app->get('/logout', function (Request $req, Response $resp) use ($twig, $securityService) {
     session_unset();
     session_destroy();
     return $resp->withStatus(302)->withHeader('location', '/Template/');//todo: change for production
 });
 
-$app->get('/contact', function (req $req, resp $resp) use ($twig){
+$app->get('/contact', function (Request $req, Response $resp) use ($twig){
     $twigVar = array(
-        "pageTitle" => "contact",
+        "pageTitle" => "Contact",
         "contact" => true
     );
     if(isset($_SESSION['user']) && $_SESSION['user']['isAdmin'])
@@ -113,13 +113,34 @@ $app->get('/contact', function (req $req, resp $resp) use ($twig){
 
     return $resp->getBody()->write($twig->render('contact.html.twig', $twigVar));
 });
-$app->post('/contact', function (req $req, resp $resp) use ($mailService){
+$app->post('/contact', function (Request $req, Response $resp) use ($mailService){
     $data = $req->getParsedBody();
 
     if(!isset($data['name']) || !isset($data['email']) || !isset($data['subject']) || !isset($data['message']) || !isset($data['g-recaptcha-response']))
         return $response = $resp->withStatus(400);
 
     return $resp->getBody()->write(json_encode($mailService->send($data['email'], $data['name'], $data['message'], $data['subject'], $data['g-recaptcha-response'])));
+});
+
+$app->group('/admin', function () use ($app, $twig, $artRepo){
+    $app->get('/upload', function(Request $req, Response $resp) use ($twig){
+        return $resp->getBody()->write($twig->render('upload.html.twig', array(
+            "pageTitle" => "Upload",
+            "admin" => true
+        )));
+    });
+    $app->post('/upload', function(Request $req, Response $resp) use ($twig, $artRepo){
+        $uploadController = new AppBundle\controllers\UploadController($artRepo);
+        $data = $req->getParsedBody();
+        return $resp->getBody()->write("hi");
+    });
+})->add(function(Request $req, Response $resp, $next) use ($securityService){
+    if($securityService->checkAdmin()){
+        return $resp = $next($req, $resp);
+    }
+    else{
+        return $response = $resp->withStatus(404);
+    }
 });
 
 $app->run();
